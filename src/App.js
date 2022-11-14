73fd4a5a5fe749c16427d462488df75a103c8abd
Shanzyk <ilya.silovo@gmail.com>
2022-11-14 12:08:41 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [error, setError] = useState(null);
-  const [errorr, setErrorr] = useState(null);
+  const [isChecked, setIsChecked] = useState(false);
 
   function isValidEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
   }
 
-  const handleChange = event => {
-    if (!isValidEmail(event.target.value)) {
-      setError('Please provide a valid e-mail address');
-    } else {
-      setError(null);
-    }
+  function isInvalidValidEmail(email) {
+    return /\S+@\S+\.co/.test(email);
+  }
 
-    if (event.target.value.trim().length == 0) {
-      setErrorr('Email address is required');
+  const handleChange = (event) => {
+    if (event.target.value.trim().length === 0) {
+      setError("Email address is required");
     } else {
-      setErrorr(null);
+      if (!isValidEmail(event.target.value)) {
+        setError("Please provide a valid e-mail address");
+      } else {
+        if (isInvalidValidEmail(event.target.value)) {
+          setError("We are not accepting subscribtions from Colombia emails");
+        } else {
+          setError(null);
+        }
+      }
     }
     setMessage(event.target.value);
-  }
+  };
+
+  const checkboxChange = (event) => {
+    setIsChecked(event.target.checked);
+    if (!isChecked) {
+      setError(null);
+    }
+  };
+
+  const buttonClick = (event) => {
+    if (!isChecked) {
+      setError("You must accept the terms and conditions");
+    }
+  };
 
   return (
     <body>
       <aside class="side">
-
         <img class="logo" src="pineapple.png"></img>
 
         <nav class="nav">
@@ -38,18 +55,47 @@ function App() {
         </nav>
         <div class="subs">
           <h1>Subscribe to newsletter</h1>
-          <p>Subscribe to our newsletter and get 10% discount on pineapple glasses.</p>
+          <p>
+            Subscribe to our newsletter and get 10% discount on pineapple
+            glasses.
+          </p>
         </div>
         <form class="form">
           <div class="emailpad">
-            <input class="emailinput" type="email" name="email" id="email-field" placeholder="Type your email address here…" value={message} onChange={handleChange}></input>
-            <input type="submit" class="icon" value="" ></input>
+            <input
+              class="emailinput"
+              type="email"
+              name="email"
+              id="email-field"
+              placeholder="Type your email address here…"
+              value={message}
+              onChange={handleChange}
+              required
+            ></input>
+            <input
+              type="submit"
+              class="icon"
+              value=""
+              onClick={buttonClick}
+            ></input>
           </div>
-          <span id="email-error" class="error">{error, errorr}</span>
+          <span id="email-error" class="error">
+            {error}
+          </span>
           <div class="checkbox">
             <label>
-              <input type="checkbox" class="large" name="checkbox" id="checkbox" required />
-              <span>I agree to <a href="#">terms of service</a></span></label>
+              <input
+                type="checkbox"
+                class="large"
+                name="checkbox"
+                id="checkbox"
+                onChange={checkboxChange}
+                required
+              />
+              <span>
+                I agree to <a href="#">terms of service</a>
+              </span>
+            </label>
           </div>
         </form>
         <hr class="line"></hr>
@@ -62,7 +108,7 @@ function App() {
       </aside>
       <img class="img" src="image.png"></img>
     </body>
-  )
+  );
 }
 
 export default App;
